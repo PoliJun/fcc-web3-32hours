d1d91979b1217a0f08a0a42ff54e631bb2558d65
PoliJun <rivenloving@gmail.com>
2024-04-22 06:39:36 +0800

    Update FundMe.test.js to use ethers.getContractAt instead of ethers.getContract

--- hardhat-fund-me/test/unit/FundMe.test.js
+++ hardhat-fund-me/test/unit/FundMe.test.js
@@ -8,14 +8,14 @@ const { developmentChains } = require("../../helper-hardhat-config");
           let fundMe;
           let mockV3Aggregator;
           let deployer;
-          const sendValue = ethers.utils.parseEther("1");
+          const sendValue = ethers.parseEther("1"); // Ethers V6
           beforeEach(async () => {
               // const accounts = await ethers.getSigners()
               // deployer = accounts[0]
               deployer = (await getNamedAccounts()).deployer;
               await deployments.fixture(["all"]);
-              fundMe = await ethers.getContract("FundMe", deployer);
-              mockV3Aggregator = await ethers.getContract(
+              fundMe = await ethers.getContractAt("FundMe", deployer);
+              mockV3Aggregator = await ethers.getContractAt(
                   "MockV3Aggregator",
                   deployer,
               );
