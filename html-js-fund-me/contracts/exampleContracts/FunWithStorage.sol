91837a9f27003f37eadf4f9b8da82e838fb69188
PoliJun <rivenloving@gmail.com>
2024-04-23 04:45:06 +0800

    Update FunWithStorage contract to return newVar in doStuff function

--- html-js-fund-me/contracts/exampleContracts/FunWithStorage.sol
+++ html-js-fund-me/contracts/exampleContracts/FunWithStorage.sol
@@ -24,9 +24,10 @@ contract FunWithStorage {
         i_not_in_storage = 123;
     }
 
-    function doStuff() public {
+    function doStuff() public view returns (uint256) {
         uint256 newVar = favoriteNumber + 1; // SLOAD
-        bool otherVar = someBool; // SLOAD
+        // bool otherVar = someBool; // SLOAD
         // ^^ memory variables
+        return newVar;
     }
 }
